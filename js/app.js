cc2ab78540afcd2432b6f8eba048a98891a4ad47
Francesco Punta <puntafrancesco@gmail.com>
2023-02-03 16:06:15 +0100

    if

--- js/app.js
+++ js/app.js
@@ -7,6 +7,19 @@ const email = ['LiberoMail', 'GMail', 'Hotmail', 'ColdMail', 'Outlook']
 console.log(email)
 
 
+// Chiedo il provider all'utente tramite il prompt
+const sceltaUtente = prompt('Seleziona il tuo email provider')
+console.log(sceltaUtente)
+
+
+// Se la scelta dell'utente corrisponde a mail disponibili:
+if  (sceltaUtente == email ){
+    console.log(" Bella Mail" )
+}
+else {
+
+}
+
 
 
 
